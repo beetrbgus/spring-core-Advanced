95179be61069418757d5a28ca49c567f9e4faa6e
beetrbgus <devbeetrb0214@gmail.com>
2023-08-05 19:43:43 +0900

    feat - 기본 Order API 생성
    - v0 이라는 이름이 붙은 것은 v1, v2처럼 여러개의 버전으로 나뉘어질 수 있기 때문에 붙는다.
    - 아이템 생성을 1초 동안 쉬는걸로 작업을 대체함

---
+++ src/main/java/com/advanced/springcoreadvanced/v0/OrderControllerV0.java
@@ -0,0 +1,18 @@
+package com.advanced.springcoreadvanced.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV0 {
+    private final OrderServiceV0 orderService;
+
+    @GetMapping("/v0/request")
+    public String saveItem(@RequestParam String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+}
