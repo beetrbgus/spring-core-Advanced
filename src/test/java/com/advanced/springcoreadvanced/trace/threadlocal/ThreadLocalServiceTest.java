a3b85bc887fb88a6a868aaf5b104f891a355ffc0
beetrbgus <devbeetrb0214@gmail.com>
2023-08-10 23:42:16 +0900

    feat - ThreadLocal로 동시성 문제 해결 예제
    - 동시성 문제를 해결한 ThreadLocalService 추가
    - 동시성 문제를 해결한 ThreadLocal 테스트 코드 추가

---
+++ src/test/java/com/advanced/springcoreadvanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,43 @@
+package com.advanced.springcoreadvanced.trace.threadlocal;
+
+import com.advanced.springcoreadvanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class ThreadLocalServiceTest {
+    private ThreadLocalService threadLocalService = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("---------------- Main Start ----------------");
+        Runnable userA = () -> {
+            threadLocalService.logic("userA");
+        };
+        Runnable userB = () -> {
+            threadLocalService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000); // 동시성 문제 발생 X
+        sleep(100); // 동시성 문제 발생 O
+        threadB.start();
+
+        sleep(3000); // 메인 쓰레드 종료 대기
+        log.info("Main Exit");
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
