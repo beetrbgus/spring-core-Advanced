76b2b54257380db08a357c9be7076a15df8d6fa8
beetrbgus <devbeetrb0214@gmail.com>
2023-08-12 17:06:51 +0900

    feat - 템플릿 메서드 패턴
    고정된 부분과 변화되는 부분을 추상 클래스로 나눠 코드 중복문제를 해결하기 위한 패턴

---
+++ src/test/java/com/advanced/springcoreadvanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,50 @@
+package com.advanced.springcoreadvanced.trace.template;
+
+import com.advanced.springcoreadvanced.trace.template.code.AbstractTemplate;
+import com.advanced.springcoreadvanced.trace.template.code.SubClassLogic1;
+import com.advanced.springcoreadvanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic2();
+        // 변하는 부분 : 비즈니스 로직
+        // 변하지 않는 부분 : 시간을 측정하는 것
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate abstractTemplate1 = new SubClassLogic1();
+        abstractTemplate1.execute();
+
+        AbstractTemplate abstractTemplate2 = new SubClassLogic2();
+        abstractTemplate2.execute();
+    }
+}
---
+++ src/test/java/com/advanced/springcoreadvanced/trace/template/code/AbstractTemplate.java
@@ -0,0 +1,20 @@
+package com.advanced.springcoreadvanced.trace.template.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public abstract class AbstractTemplate {
+
+    public void execute() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        call();
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    protected abstract void call();
+}
