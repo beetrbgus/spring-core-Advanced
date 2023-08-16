83cda7bbe8ae4bd217a2db6c21fa228388389c1e
beetrbgus <devbeetrb0214@gmail.com>
2023-08-16 23:08:38 +0900

    feat - 템플릿 콜백 패턴
    - CallBack으로 변하지 않는 코드를 인터페이스화 시킴

---
+++ src/main/java/com/advanced/springcoreadvanced/trace/callback/TraceCallBack.java
@@ -0,0 +1,5 @@
+package com.advanced.springcoreadvanced.trace.callback;
+
+public interface TraceCallBack<T> {
+    T call();
+}
---
+++ src/test/java/com/advanced/springcoreadvanced/trace/strategy/code/template/Callback.java
@@ -0,0 +1,5 @@
+package com.advanced.springcoreadvanced.trace.strategy.code.template;
+
+public interface Callback {
+    void call();
+}
