30dfb71d2c75ff83a47c3709ba63de035a6a2636
beetrbgus <devbeetrb0214@gmail.com>
2023-08-19 03:38:49 +0900

    feat - 프록시 패턴을 위한 기본 설정
    - Controller 추가
    - service 추가
    - repository 추가

---
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1Impl.java
@@ -0,0 +1,22 @@
+package hello.proxy.app.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+public class OrderControllerV1Impl implements OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+
+    @Override
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @Override
+    public String noLog() {
+        return "ok";
+    }
+}
