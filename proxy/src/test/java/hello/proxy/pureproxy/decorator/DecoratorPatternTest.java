a40e7706c3a6b8dfe1dbcfabd7b606a5f8a3bf96
beetrbgus <devbeetrb0214@gmail.com>
2023-08-25 20:14:25 +0900

    feat - 데코레이터 패턴
    - 실행 시간을 측정하는 기능 추가
    - 클라이언트는 코드를 변경하지 않고 프록시를 원하는 만큼 집어넣어 실행시킬 수 있음.

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -4,6 +4,7 @@ import hello.proxy.pureproxy.decorator.code.Component;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
 import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.TimeDecorator;
 import org.junit.jupiter.api.Test;
 
 public class DecoratorPatternTest {
@@ -20,6 +21,17 @@ public class DecoratorPatternTest {
         Component messageComponent = new MessageDecorator(realComponent);
 
         DecoratorPatternClient client = new DecoratorPatternClient(messageComponent);
+
+        client.execute();
+    }
+
+    @Test
+    void DecoratorTest2() {
+        Component realComponent = new RealComponent();
+        Component messageComponent = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageComponent);
+
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
 
         client.execute();
     }
